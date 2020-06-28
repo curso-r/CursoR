63e983f25190989e92290649ca81c4ef5e173bca
Athos Damiani <athos.damiani@gmail.com>
2020-06-28 12:58:35 -0300

    -skimr duplicado

--- R/instalar_dependencias.R
+++ R/instalar_dependencias.R
@@ -30,7 +30,7 @@ instalar_dependencias <- function() {
       "tidyverse", "tidymodels", "rmarkdown", "knitr",
       "ISLR", "glmnet", "xgboost", "randomForest",
       "ranger", "rpart", "rpart.plot", "pROC", "vip", "modeldata",
-      "usemodels", "tidypredict", "jpeg", "MASS", "skimr", "DataExplorer",
+      "usemodels", "tidypredict", "jpeg", "MASS", "DataExplorer",
       "skimr", "naniar", "patchwork"
     ),
     github = c("RobertMyles/modelscript", "allisonhorst/palmerpenguins")
